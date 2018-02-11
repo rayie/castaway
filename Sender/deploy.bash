8ff2da04f6ef7d18312653cef347319f765ad21f
Raymond Ie <ie.raymond@gmail.com>
2018-02-11 14:51:17 -0800

    rie

--- Sender/deploy.bash
+++
@@ -1,3 +0,0 @@
-#gsutil rm gs://castaway-receiver/receiver.html
-gsutil -h "Cache-Control:public,no-cache,must-revalidate" -h "Content-Type:text/html" cp sender.html gs://song-chords/sender.html
-gsutil acl ch -u AllUsers:R gs://song-chords/sender.html
